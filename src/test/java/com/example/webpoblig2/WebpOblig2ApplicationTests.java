e09da7bc0602f768a6782bac1ce55069bee4db70
Rankatanka <hegro8348@oslomet.no>
2024-04-03 09:26:30 +0200

    Commit message

---
+++ src/test/java/com/example/webpoblig2/WebpOblig2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webpoblig2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebpOblig2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
